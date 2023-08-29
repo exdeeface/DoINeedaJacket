0d1fb7401c86bcd708772810f81e7dc3cbbcdf4a
exdeeface <bowmang@lsbu.ac.uk>
2023-08-29 09:07:48 +0100

    testing jacoco

--- src/test/java/com/grae/WeatherTest/WeatherTestApplicationTest.java
+++ src/test/java/com/grae/WeatherTest/WeatherTestApplicationTest.java
@@ -3,7 +3,7 @@ package com.grae.WeatherTest;
 import org.junit.jupiter.api.Test;
 
 class WeatherTestApplicationTest {
-
+    //comment
     @Test
     void requestFakeDailyForecast() {
         assert(true); //jacoco test
